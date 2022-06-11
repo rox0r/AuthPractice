92aeca9568ce3704640de3509907bac715e154e2
Raunaq singh <macbook@Raunaqs-MacBook-Air.local>
2022-06-11 12:17:51 +0530

    Encrypt with Mongoose-encryption and .env variable

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -18,6 +19,12 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+});
+
+const secretStr = process.env.SECRET;
+userSchema.plugin(encrypt, {
+  secret: secretStr,
+  encryptedFields: ["password"],
 });
 
 const User = new mongoose.model("User", userSchema);
